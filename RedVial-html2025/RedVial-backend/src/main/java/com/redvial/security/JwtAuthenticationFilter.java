eeb000aa5f45d5cd8eaa8c525610a9e6d265ab95
JPcoronado3 <juanpablocoronado535@gmail.com>
2025-11-26 22:06:33 -0500

    Update JwtAuthenticationFilter.java

--- RedVial-html2025/RedVial-backend/src/main/java/com/redvial/security/JwtAuthenticationFilter.java
+++ RedVial-html2025/RedVial-backend/src/main/java/com/redvial/security/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
